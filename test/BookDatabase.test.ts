d447bd8bd9f6dce90fc70d3f320af1dd435bc542
Theobhg <assis.theo@gmail.com>
2025-10-02 14:55:00 -0300

    test: Enhance BookDatabase tests with add, edit, and remove book functionalities, including permission checks.

--- test/BookDatabase.test.ts
+++ test/BookDatabase.test.ts
@@ -20,4 +20,37 @@ describe("BookDatabase", function () {
     const count = await bookDatabase.count();
     expect(count).to.equal(0);
   });
+
+  it("Should add book", async function () {
+    const { bookDatabase, owner, otherAccount } = await loadFixture(deployFixture);
+    await bookDatabase.registerBook({ title: 'Livro 1', year: 2026})
+    const count = await bookDatabase.count();
+    expect(count).to.equal(1);
+  });
+
+  it("Should edit book", async function () {
+    const { bookDatabase, owner, otherAccount } = await loadFixture(deployFixture);
+    await bookDatabase.registerBook({ title: 'Livro 1', year: 2026})
+    await bookDatabase.editBook(1, { title: 'Livro 2', year: 2026 })
+    
+    const book =  await bookDatabase.books(1)
+    expect(book.title).to.equal('Livro 2');
+  });
+
+  it("Should remove book", async function () {
+    const { bookDatabase, owner, otherAccount } = await loadFixture(deployFixture);
+    await bookDatabase.registerBook({ title: 'Livro 1', year: 2026})
+    await bookDatabase.removeBook(1)
+    
+    const count =  await bookDatabase.count()
+    expect(count).to.equal(0);
+  });
+
+  it("Should NOT remove book", async function () {
+    const { bookDatabase, owner, otherAccount } = await loadFixture(deployFixture);
+    const instance = bookDatabase.connect(otherAccount)
+
+    await bookDatabase.registerBook({ title: 'Livro 1', year: 2026})
+    await expect(instance.removeBook(1)).to.be.revertedWith("You don't have permission.");
+  });
 });
